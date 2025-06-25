5f0951161cabd625d2dfcf738510bb57c9dedda7
sylvan <sylvanraine@gmail.com>
2025-06-25 13:54:20 +0800

    trying axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{debug_handler, routing, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/", routing::get(index));
+    let listener = TcpListener::bind(("127.0.0.1", 8080)).await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, router).await.unwrap();
+}
+
+#[debug_handler]
+async fn index() -> &'static str {
+    "Hello world!"
 }
